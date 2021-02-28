c168b5ecfe9d8b2c8764e49b0d131cbace3850ae
Sam <Samstep2012@gmail.com>
2021-02-27 17:34:26 -0800

    error handling | bug fixes

--- README.md
+++ README.md
@@ -1,5 +1,49 @@
-# Speaking Detector for OBS
+# Discord Speaking Detector for OBS
 
-### Detects if specific users are speaking and toggles a source within OBS.
+### Detects if specific users are speaking and toggles a specific source within OBS.
+
+## Installation
+1. Download [Node.JS](https://nodejs.org/en/) `14.16.0 LTS` was used for development
+2. Clone the repo `git clone https://github.com/Saamstep/discord-speak-detector-obs.git`
+3. Enter the installation directory `cd discord-speak-detector-obs/`
+4. Install node modules `npm install`
+
+## Setup
+1. Copy the config and fill out as desired `cp example.config.json config.json`
+```json
+{
+    "token": "", //Bot token
+    "guild": "", //Guild ID
+    "liveChannelID": "", //Voice channel to detect speaking in
+    "prefix": "-", //Bot prefix
+    "perms": "", //Admin role name or role ID
+    "casters": [""], //List of Discord User ID's you want to detect (a user who is not on this list wil not be detected)
+    "scene_name": "", //The name of the specific scene to toggle sources in (multi-scene support coming soon)
+    "websocket": "localhost:4444", //Websocket address
+    "password": "" //If you have a password for your obs websocket enter it here or leave this blank
+}
+```
+2. In OBS the source name for each user must be equal to the specific user's Discord ID and this source __must__ be in the scene specified in the config
+
+## Example
+If we had the following users we wanted to detect:
+`1234`
+`8765`
+
+The **casters** array in the `config.json` must be `["1234", "8765"]`
+In OBS I would create two scenes of ANY type (only the name of the scene matters) and name them accordingly 
+![obs screenshot](https://i.imgur.com/zgZpfpF.png)
+
+## Usage
+### Start
+Run `npm start`
+
+### Stop
+Run `npm stop`
+
+## Notes
+* You can close the terminal window after starting app if desired.
+
+* To Open Bot Logs: `npm logs`
 
 
--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const client = new Discord.Client();
 client.config = require('./config.json');
 
 const connect = (live) => { 
+    assert(live != null);
     live.join().then(c => c.voice.setSelfMute(true));
-    console.log(`Connected to ${live.name}`);
+    console.log(`[Bot] Connected to ${live.name}`);
 }
 
 const disconnect = (live) => {
-    assert(live != null);
     live.leave();
-    console.log("Disconnected from voice channel.");
+    console.log("[Bot] Disconnected from voice channel.");
 }
 
 const permCheck = (msg) => {
@@ -23,10 +23,14 @@ const permCheck = (msg) => {
     return msg.member.roles.cache.has(msg.guild.roles.cache.find(r => r.name == client.config.perms || r.id == client.config.perms).id);
 }
 
-obs.connect({address: client.config.websocket});
+obs.connect({address: client.config.websocket}).catch(e => console.error("[OBS] " + e.error));
 
 obs.on('ConnectionClosed', (data) => {
-    obs.connect({address: client.config.websocket, password: client.config.password});
+    console.log("[OBS] Retrying in 10 seconds...");
+    setTimeout(() => {
+        obs.connect({address: client.config.websocket}).catch(e => console.error("[OBS] " + e.error));
+    }, 10000);
+
 });
 
 obs.on('ConnectionOpened', (data) => {
@@ -34,10 +38,11 @@ obs.on('ConnectionOpened', (data) => {
 });
 
 obs.on('AuthenticationFailure', (data) => {
-    return console.log("[OBS] Authentication to the websocket has failed please try again.");
+    console.log("[OBS] Authentication to the websocket has failed please try again.");
+    process.exit(1);
 })
 
-client.on('ready', async () => {
+client.on('ready', () => {
     let guild = client.guilds.cache.get(client.config.guild);
     let vc =  guild.channels.cache.get(client.config.liveChannelID);
 
@@ -48,8 +53,10 @@ client.on('ready', async () => {
 
     for(i in client.config.casters) {
         assert(client.config.casters != null);
-        obs.send("SetSceneItemRender", {"scene-name": client.config.scene_name, source: client.config.casters[i], render: false});
+        obs.send("SetSceneItemRender", {"scene-name": client.config.scene_name, source: client.config.casters[i], render: false}).catch(e => console.error("[OBS] " + e.error));
     }
+
+    client.user.setPresence({status: 'invisible'});
 
     console.log("[Bot] Initalized");
 });
@@ -71,14 +78,14 @@ client.on('message', msg => {
 });
 
 client.on('guildMemberSpeaking', (member, speaking) => {
-    let guild = client.guilds.cache.get(client.config.guild);
+    let guild = client.guilds.cache.get(client.config.guild).catch(e => console.error("[Bot] " + e));
     assert(guild != null);
 
     if(member.guild.id != guild.id) return;
     if(client.config.casters.some(usr => member.user.id == usr)) {
-        obs.send("SetSceneItemRender", {"scene-name": client.config.scene_name, source: member.user.id, render: speaking.bitfield == 1});
+        obs.send("SetSceneItemRender", {"scene-name": client.config.scene_name, source: member.user.id, render: speaking.bitfield == 1}).catch(e => console.error("[OBS] " + e.error));
     }
 });
 
-client.login(client.config.token);
+client.login(client.config.token).catch(e => console.error("[Bot] " + e));
 
